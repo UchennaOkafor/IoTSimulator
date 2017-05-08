77c790c4b2b6c17b2c31566ad2c87324e81ed107
uche1 <okafor.u@hotmail.com>
2017-05-08 20:08:00 +0100

    * Swapped out JavaScriptSerializer for JSON.NET for better performance and increased productivity and reliability

--- IoTSimulator/Program.cs
+++ IoTSimulator/Program.cs
@@ -5,16 +5,6 @@ namespace IoTSimulator
 {
     class Program
     {
-        //if device is off, do NOT send data. 
-
-        //get a list of devices that are on, and simulate all of them
-
-        //Thermostat attributes are temperate and humidity
-
-        //var jss = new JavaScriptSerializer();
-        //var json = jss.Serialize(new LightBulb());
-        //var lb = jss.Deserialize<LightBulb>("{\"Temperate\":45,\"Id\":3}");
-
         static void Main(string[] args)
         {
             Console.Title = "IoT Simulator";
